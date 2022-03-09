f2a60430364fb2e36d3eebff333d603bdfc22a81
Luke Carr <24438483+lukecarr@users.noreply.github.com>
2022-03-09 13:54:10 +0000

    fix: added default to download button (prevent flashing)

--- hooks/useDownload.ts
+++ hooks/useDownload.ts
@@ -6,21 +6,21 @@ import useOS from './useOS'
 const fetcher = (url: RequestInfo, args?: RequestInit) => fetch(url, args).then(res => res.json())
 
 export default function useDownload() {
-  const [url, setUrl] = useState('')
-  const [filename, setFilename] = useState(['', ''])
+  const [url, setUrl] = useState('#')
+  const [filename, setFilename] = useState(['macOS', ''])
   const os = useOS()
   const { data } = useSWR('/api/release', fetcher)
 
   useEffect(() => {
     switch (os) {
-      case 'mac':
-        setFilename(['macOS', 'Lapce-macos.dmg'])
+      case 'win':
+        setFilename(['Windows', 'Lapce-windows.msi'])
         break
       case 'linux':
         setFilename(['Linux', 'Lapce-linux.tar.gz'])
         break
       default:
-        setFilename(['Windows', 'Lapce-windows.msi'])
+        setFilename(['macOS', 'Lapce-macos.dmg'])
     }
   }, [os])
 
